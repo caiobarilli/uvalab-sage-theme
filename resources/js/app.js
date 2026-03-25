6f6c3a48842f2c39a80de776616e6118bfec5c2c
Caio Barilli <caiobarilli@gmail.com>
2026-03-25 13:32:02 -0300

    Add HeroSlider Livewire component and integrate Swiper for a responsive slider

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 
+import Swiper from 'swiper'
+import { Pagination, Autoplay } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Swiper('.hero-slider', {
+        modules: [Pagination, Autoplay],
+        loop: true,
+        autoplay: {
+            delay: 5000,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    })
+})
