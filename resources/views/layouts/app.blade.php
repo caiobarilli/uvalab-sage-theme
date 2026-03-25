cee3ec87fd792a202462c7b991d6c69ef2bd1e4a
Caio Barilli <caiobarilli@gmail.com>
2026-03-25 11:06:59 -0300

    Add Livewire Flux dependency and integrate Flux components in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
+    @fluxAppearance
 </head>
 
 <body @php(body_class())>
@@ -22,7 +23,30 @@
         @include('sections.header')
 
         <main id="main" class="main">
+
+            <flux:button variant="primary" color="zinc">Zinc</flux:button>
+            <flux:button variant="primary" color="red">Red</flux:button>
+            <flux:button variant="primary" color="orange">Orange</flux:button>
+            <flux:button variant="primary" color="amber">Amber</flux:button>
+            <flux:button variant="primary" color="yellow">Yellow</flux:button>
+            <flux:button variant="primary" color="lime">Lime</flux:button>
+            <flux:button variant="primary" color="green">Green</flux:button>
+            <flux:button variant="primary" color="emerald">Emerald</flux:button>
+            <flux:button variant="primary" color="teal">Teal</flux:button>
+            <flux:button variant="primary" color="cyan">Cyan</flux:button>
+            <flux:button variant="primary" color="sky">Sky</flux:button>
+            <flux:button variant="primary" color="blue">Blue</flux:button>
+            <flux:button variant="primary" color="indigo">Indigo</flux:button>
+            <flux:button variant="primary" color="violet">Violet</flux:button>
+            <flux:button variant="primary" color="purple">Purple</flux:button>
+            <flux:button variant="primary" color="fuchsia">Fuchsia</flux:button>
+            <flux:button variant="primary" color="pink">Pink</flux:button>
+            <flux:button variant="primary" color="rose">Rose</flux:button>
+
+
             <livewire:quote />
+
+
             @yield('content')
         </main>
 
@@ -38,6 +62,7 @@
     @php(do_action('get_footer'))
     @php(wp_footer())
     @livewireScripts
+    @fluxScripts
 </body>
 
 </html>
