7d5ca3843f8fe2996312a6dd47797e9da0e37d14
Caio Barilli <caiobarilli@gmail.com>
2026-03-28 10:38:43 -0300

    Add sidebar navigation to admin layout for improved accessibility

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,6 +10,42 @@
 
 <body>
     <div id="admin-app">
+
+
+        <flux:sidebar sticky collapsible class="bg-zinc-50  border-r border-zinc-200  ">
+            <flux:sidebar.header>
+                <flux:sidebar.brand href="#" logo="{{ Vite::asset('resources/images/uva-logo.png') }}"
+                    name="UvaLab." />
+                <flux:sidebar.collapse
+                    class="in-data-flux-sidebar-on-desktop:not-in-data-flux-sidebar-collapsed-desktop:-mr-2" />
+            </flux:sidebar.header>
+
+            <flux:sidebar.nav>
+                <flux:sidebar.item icon="home" href="/uvalab-admin" current>
+                    Theme
+                </flux:sidebar.item>
+
+                <flux:sidebar.group expandable icon="star" heading="Sliders" class="grid">
+                    <flux:sidebar.item href="{{ admin_url('edit.php?post_type=hero_slide') }}" target="_parent">
+                        Hero Slides
+                    </flux:sidebar.item>
+                </flux:sidebar.group>
+            </flux:sidebar.nav>
+
+            <flux:sidebar.spacer />
+
+            <flux:sidebar.nav>
+                <flux:sidebar.item icon="cog-6-tooth" href="#">Settings</flux:sidebar.item>
+                <flux:sidebar.item icon="information-circle" href="#">Help</flux:sidebar.item>
+            </flux:sidebar.nav>
+
+        </flux:sidebar>
+
+        <flux:header class="lg:hidden">
+            <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+            <flux:spacer />
+        </flux:header>
+
         @yield('content')
     </div>
 
