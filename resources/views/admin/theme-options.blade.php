d1b7c69839b13a2a92a324a091af7b675d2da1af
Caio Barilli <caiobarilli@gmail.com>
2026-03-29 08:50:01 -0300

    Add My Account page status notifications in theme options

--- resources/views/admin/theme-options.blade.php
+++ resources/views/admin/theme-options.blade.php
@@ -46,6 +46,51 @@
                 </flux:callout>
             @endif
 
+            @php
+                $myaccount_page_id = get_option('woocommerce_myaccount_page_id');
+                $myaccount_page = $myaccount_page_id ? get_post($myaccount_page_id) : null;
+                $page_content = $myaccount_page ? $myaccount_page->post_content : '';
+                $uses_uvalab = str_contains($page_content, 'uvalab_my_account');
+                $uses_woo = str_contains($page_content, 'woocommerce_my_account');
+            @endphp
+
+            @if ($myaccount_page)
+                @if ($uses_uvalab)
+                    <flux:callout icon="check-circle" variant="success" inline>
+                        <flux:callout.heading>My Account page is using Uvalab</flux:callout.heading>
+                        <flux:callout.text>The page "{{ $myaccount_page->post_title }}" is using
+                            <code>[uvalab_my_account]</code>.</flux:callout.text>
+                    </flux:callout>
+                @elseif($uses_woo)
+                    <flux:callout icon="exclamation-triangle" variant="warning" inline>
+                        <flux:callout.heading>My Account page is using WooCommerce default</flux:callout.heading>
+                        <flux:callout.text>Replace <code>[woocommerce_my_account]</code> with
+                            <code>[uvalab_my_account]</code> in the My Account page.</flux:callout.text>
+                        <x-slot name="actions">
+                            <flux:button href="{{ admin_url('post.php?post=' . $myaccount_page_id . '&action=edit') }}"
+                                target="_parent">
+                                Edit My Account page
+                            </flux:button>
+                        </x-slot>
+                    </flux:callout>
+                @else
+                    <flux:callout icon="exclamation-triangle" variant="warning" inline>
+                        <flux:callout.heading>My Account page has no shortcode</flux:callout.heading>
+                        <flux:callout.text>Add <code>[uvalab_my_account]</code> to the My Account page.</flux:callout.text>
+                        <x-slot name="actions">
+                            <flux:button href="{{ admin_url('post.php?post=' . $myaccount_page_id . '&action=edit') }}"
+                                target="_parent">
+                                Edit My Account page
+                            </flux:button>
+                        </x-slot>
+                    </flux:callout>
+                @endif
+            @else
+                <flux:callout icon="exclamation-triangle" variant="danger" inline>
+                    <flux:callout.heading>My Account page not found</flux:callout.heading>
+                    <flux:callout.text>WooCommerce My Account page is not configured.</flux:callout.text>
+                </flux:callout>
+            @endif
         </div>
 
         <flux:separator variant="subtle" class="my-6" />
