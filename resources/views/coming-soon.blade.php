dbe8300a40257cb5f6ee5b5d83db80c0c2635ca3
Caio Barilli <caiobarilli@gmail.com>
2026-03-29 07:32:22 -0300

    Refactor Coming Soon page structure to use standard HTML layout and improve readability

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -1,17 +1,37 @@
-@extends('layouts.app')
+<!doctype html>
+<html @php(language_attributes())>
 
-@section('content')
-    <div class="flex min-h-[60vh] flex-col items-center justify-center text-center px-4">
-        <img src="{{ Vite::asset('resources/images/uva-logo.png') }}" alt="Uvalab" class="h-16 mb-8 opacity-80">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @php(do_action('get_header'))
+    @php(wp_head())
 
-        <flux:heading size="xl" class="mb-4">We're coming soon</flux:heading>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
 
-        <flux:text class="max-w-md mb-8">
-            Our store is currently under construction. We'll be back soon with something amazing.
-        </flux:text>
+<body @php(body_class())>
+    @php(wp_body_open())
 
-        @if (is_user_logged_in())
-            <flux:button href="/" variant="primary">Back to Home</flux:button>
-        @endif
+    <div id="app">
+        <main id="main" class="main">
+            <div class="flex min-h-[60vh] flex-col items-center justify-center text-center px-4">
+                <img src="{{ Vite::asset('resources/images/uva-logo.png') }}" alt="Uvalab" class="h-16 mb-8 opacity-80">
+
+                <flux:heading size="xl" class="mb-4">We're coming soon</flux:heading>
+
+                <flux:text class="max-w-md mb-8">
+                    Our store is currently under construction. We'll be back soon with something amazing.
+                </flux:text>
+            </div>
+        </main>
     </div>
-@endsection
+
+    @php(do_action('get_footer'))
+    @php(wp_footer())
+    @livewireScripts
+    @fluxScripts
+</body>
+
+</html>
