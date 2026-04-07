4fcd968e286fdedef94b747605da488dca794729
Caio Barilli <caiobarilli@gmail.com>
2026-04-07 12:12:49 -0300

    feat: remove uvalab_menu shortcode and replace with wp:navigation for improved header structure

--- app/Providers/ShortcodesServiceProvider.php
+++ app/Providers/ShortcodesServiceProvider.php
@@ -22,29 +22,5 @@ class ShortcodesServiceProvider extends ServiceProvider
         add_shortcode('uvalab_my_account', function () {
             return Livewire::mount('customer.dashboard');
         });
-
-        add_shortcode('uvalab_menu', function ($atts) {
-            $atts = shortcode_atts([
-                'location' => 'primary_navigation',
-                'class' => 'site-header__nav',
-            ], $atts);
-
-            if (! has_nav_menu($atts['location'])) {
-                return '<nav class="'.esc_attr($atts['class']).'" id="site-nav">'
-                    .'<a href="'.esc_url(admin_url('nav-menus.php')).'">'
-                    .esc_html__('Defina o menu em Primary Navigation', 'sage')
-                    .'</a>'
-                    .'</nav>';
-            }
-
-            return wp_nav_menu([
-                'theme_location' => $atts['location'],
-                'container' => 'nav',
-                'container_class' => $atts['class'],
-                'container_id' => 'site-nav',
-                'fallback_cb' => false,
-                'echo' => false,
-            ]);
-        });
     }
 }
